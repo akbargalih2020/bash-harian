b4f36433a134b1bee18bed87660eb77e1999fd49
akbargalih2020 <102049028+akbargalih2020@users.noreply.github.com>
2023-03-13 12:24:49 +0700

    Update auto_global_protect.sh
    checking status login every 1 minute

--- auto_global_protect.sh
+++ auto_global_protect.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 gp-connect() {
-  osascript << EOF
-tell application "System Events" to tell process "GlobalProtect"
-	click menu bar item 1 of menu bar 2 -- Activates the GlobalProtect "window" in the menubar
-	set frontmost to true -- keep window 1 active
-	tell window 1
-	  -- Find the status
-	  tell (first UI element whose title is "Connect") to if exists then set message to "VPN is connecting now..."
-	  tell (first UI element whose title is "Disconnect") to if exists then set message to "VPN is connected."
-	  -- Click connect button
-	  tell (first UI element whose title is "Connect") to if exists then click
-	end tell
-	click menu bar item 1 of menu bar 2 -- This will close the GlobalProtect "window" after clicking Connect/Disconnect. This is optional.
-	say message
-	return message
-end tell
+osascript <<EOF
+  tell application "System Events" to tell process "GlobalProtect"
+    click menu bar item 1 of menu bar 2 -- Activates the GlobalProtect "window" in the menubar
+    set frontmost to true -- keep window 1 active
+    tell window 1
+      -- Click on the connect or disconnect button, depending on if they exist or not
+      if exists (first UI element whose title is "Connect") then
+        tell (first UI element whose title is "Connect") to if exists then click
+      else
+        tell (first UI element whose title is "Disconnect") to if exists then set message to "VPN is connecting."
+      end if
+    end tell
+    say message
+    return message
+    click menu bar item 1 of menu bar 2 -- This will close the GlobalProtect "window" after clicking Connect/Disconnect. This is optional.
+  end tell
 EOF
 }
 
@@ -55,58 +56,58 @@ case $# in
 esac
 }
 try_gp() {
-  #./gp-connect.sh > output.txt
-  gp-connect >& output.txt
+  output=$(gp-connect)
   while true; do
-    if cat output.txt | grep "connect"; then
+    if echo $output | grep "connect"; then
         echo "vpn connected"
         #exit
         break
     else
          echo "vpn not connected yet, try again;"
-         gp-connect >& output.txt
-         sleep 20
+         output=$(gp-connect)
+         sleep 30
     fi
   done
 }
 check_gp() {
- if ping 10.62.205.26 | head -3 | grep "Request timeout"; then
-   echo "vpn not connected. reconnect;"
-   try_gp ;
- else
-   echo "vpn still connected"
- fi
+  if pgrep -l Global | grep [G]lobalProtect; then
+    echo "masih login"
+  else
+    echo "sudah logout"
+    read -p "re-login? (y/n) " yn
+      case $yn in
+        y ) echo "re-login now"
+            try_gp ;;
+        n ) echo "ok, see you."
+            exit ;;
+        * ) echo "invalid response"
+            ;;
+      esac
+  fi
 }
+
 prompt() {
  read -p "running globalprotect? (y/n) " yn
     case $yn in
       y ) echo "ok, proceed now;"
-#        try_gp
-         ;;
+#          try_gp
+          ;;
       n ) echo "ok, see you;"
-        exit ;;
+          exit ;;
       * ) echo "invalid response"
     esac
 }
 n=0
 prompt;
 while true ; do
-  if ps -ef | grep -E "[G]lobalProtect" ; then
-    if [ $n == 1 ] ; then
-        echo "sudah 2 jam"
-        n=0
-        tell_gp stop
-        exit
-    else
-        echo "belum 2 jam"
-        n=$(( $n +1 ))
-        check_gp;
-        sleep 1800
-    fi
+  if pgrep -l Global | grep [G]lobalProtect; then
+    echo "masih ada proses GlobalProtect,cek status;"
+    check_gp;
+    sleep 60
   else
     echo "belum ada proses globalprotect,start proses!"
     tell_gp start
-    sleep 30
+    sleep 10
     try_gp;
   fi
 done
